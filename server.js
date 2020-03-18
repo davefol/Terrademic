3e679c844a24caf7f0122eb9b73301961afba5b2
Dave Fol <dof5@cornell.edu>
2020-03-18 00:12:01 -0400

    Added current players pane

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var http = require('http');
 var path = require('path');
-var socketIO = require('socket.io');var app = express();
+var socketIO = require('socket.io');
+var app = express();
+app.use(cookieParser());
 var server = http.Server(app);
 var io = socketIO(server);app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));// Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
+	console.log(request.cookies)
 });// Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
@@ -149,6 +153,6 @@ io.on('connection', function(socket) {
 	});
 
 	setInterval(function() {
-		io.sockets.emit('state', players);
+		io.sockets.emit('players', players);
 	}, 1000 / 5);
 })
